298e49d76fcd000d9fe92a596318ab88b4424dcb
anthstone <71052305+anthstone@users.noreply.github.com>
2020-09-23 18:47:42 -0400

    Add Prototype spreadsheet

--- EQQuantGUI.py
+++ EQQuantGUI.py
@@ -5,3 +5,33 @@ Created on Tue Sep 22 21:29:23 2020
 @author: antho
 """
 
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("EduQuant")
+
+
+Canvas1 = tk.Canvas()
+
+Canvas1.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
