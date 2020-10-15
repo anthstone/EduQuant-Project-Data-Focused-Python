d329bd7726d397e776e0d1889e8ff6e1b2ffe920
Alexander Talbott <atalbott13@gmail.com>
2020-10-15 19:56:35 -0400

    added comments

--- src/eq_data.py
+++ src/eq_data.py
@@ -8,22 +8,29 @@
 import pandas as pd
 from pathlib import Path
 
-
+# retrieve financial statistics from the data file
 def get_finance_stats(ticker):
+    # check if it exists
     try:
         path = Path(__file__).parent.absolute().parent
         df = pd.read_csv(path / "data" / "finance_stats.csv", index_col=0)
+        # return as a dict
         return df.loc[ticker.upper()].to_dict()
+    # if it doesn't, print a message and exit gracefully
     except FileNotFoundError:
         print("Data is missing. Try running 'Update Data' from the main menu")
         return None
 
 
+# retrieve closing prices from the data file
 def get_closing_prices(ticker):
+    # check if it exists
     try:
         path = Path(__file__).parent.absolute().parent
         df = pd.read_csv(path / "data" / "closing_prices.csv", index_col=0)
+        # return as a list
         return list(df.loc[ticker.upper()].items())
+    # if it doesn't, print a message and exit gracefully
     except FileNotFoundError:
         print("Data is missing. Try running 'Update Data' from the main menu")
         return None
