fc64ae4e6135480d24ddc54af889b5ee05d64b21
Alexander Talbott <atalbott13@gmail.com>
2020-10-15 19:39:38 -0400

    commented eq_simulator

--- src/eq_simulator.py
+++ src/eq_simulator.py
@@ -13,14 +13,16 @@ from pathlib import Path
 
 
 def sell_stock(ticker, portfolio):
+    # check if the ticker is in the portfolio
     if ticker.upper() not in portfolio.index:
         print("-----------Ticker not in portfolio")
         print()
         print()
+    # if not, sell it and add the revenue to Cash
     else:
         price = portfolio.loc[ticker.upper()]["Current Price"]
         portfolio.at["Cash", "Current Price"] += price
-        # profit
+        # calculate profit
         profit = (
             # round to 2 places
             round(
@@ -46,10 +48,12 @@ def purchase_stock(ticker, portfolio):
         print("-----------Ticker already in portfolio")
         print()
         print()
+    # if not, buy it (if there is enough Cash)
     elif closing_price > portfolio.loc["Cash"]["Current Price"]:
         print("-----------Not enough cash!")
         print()
         print()
+    # create a new portfolio if there isn't one
     else:
         today = date.today().strftime("%B %d, %Y")
         portfolio.loc[ticker.upper()] = {
@@ -60,6 +64,7 @@ def purchase_stock(ticker, portfolio):
             "Change since purchase": 0.0,
         }
 
+        # subtract purchase price from cash
         portfolio.at["Cash", "Current Price"] -= closing_price
 
     save_portfolio(portfolio)
@@ -67,12 +72,16 @@ def purchase_stock(ticker, portfolio):
     return portfolio
 
 
+# update the portfolio with current closing prices, dates, and change in price since purchase
 def update_portfolio(portfolio):
     today = datetime.today()
     for idx, row in portfolio.iterrows():
         if idx != "Cash":
+            # calculate how long the stock has been held
             d = datetime.strptime(row["Date Purchased"], "%B %d, %Y")
             time_held = (today - d).days
+            # get the current price of the stock from the data file
+            # closing_prices.csv
             current_price = eq_data.get_closing_prices(idx)[0][1]
             change = current_price - row["Purchase Price"]
 
@@ -84,10 +93,13 @@ def update_portfolio(portfolio):
 
 
 def load_portfolio():
+    # check for the existence of a portfolio data file
     try:
         path = Path(__file__).parent.absolute().parent
         portfolio = pd.read_csv(path / "data" / "portfolio.csv", index_col="Ticker")
+        # update it to the current data
         portfolio = update_portfolio(portfolio)
+    # if it doesn't exist, start an empty one with $10,000 cash
     except:
         print("No portfolio yet. You have $10,000 to start buying some stocks.")
         portfolio = pd.DataFrame(
@@ -113,6 +125,7 @@ def load_portfolio():
     return portfolio
 
 
+# save the portfolio to a data file
 def save_portfolio(portfolio):
     path = Path(__file__).parent.absolute().parent
     portfolio.to_csv(path / "data" / "portfolio.csv")
@@ -120,6 +133,7 @@ def save_portfolio(portfolio):
 
 def print_sim_menu():
     portfolio = load_portfolio()
+    # main event loop
     while True:
         print("EduQuant Stock Simulator")
         print()
