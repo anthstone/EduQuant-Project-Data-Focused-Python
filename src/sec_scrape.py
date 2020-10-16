0a4c998a2b2fc9330e342d16427c62166cf815d9
anthstone <71052305+anthstone@users.noreply.github.com>
2020-10-15 23:16:21 -0400

    SEC Update

--- src/sec_scrape.py
+++ src/sec_scrape.py
@@ -43,7 +43,7 @@ def add_file_to_sec_fsds(url):
     path = Path(__file__).parent.absolute().parent
     df = pd.read_csv(path / "data" / "sec_fsds.csv", sep=",", encoding="utf-8")
     new_data = df.append(
-        pd.DataFrame([[url[66:72] + ".zip", 0, 0, 0, 0, 0, 0]], columns=df.columns)
+        pd.DataFrame([[url[66:72] + ".zip", False, False, False, False, False, False]], columns=df.columns)
     )
     new_data.to_csv(path / "data" / "sec_fsds.csv", index=False)
     df.close()
@@ -102,7 +102,7 @@ def download_new_sec_file(url):
     )
     req = requests.get(url, stream=True)
     path = Path(__file__).parent.absolute().parent
-    data_loc = open(path / "data" / "sec_data_temp/" + url[66:72] + ".zip", "wb",)
+    data_loc = open(path / "data" / "sec_data_temp/" / (url[66:72] + ".zip"), "wb")
     for chunk in req.iter_content(chunk_size=512):
         if chunk:
             data_loc.write(chunk)
@@ -125,7 +125,7 @@ def process_data_file(process_new_data, url):
         },
     )
     sec_fsds_array = np.array(sec_fsds_data)
-
+    sec_fsds_data.close()
     company_list = pd.read_csv(
         path / "data" / "company_list.csv",
         dtype={
@@ -135,7 +135,7 @@ def process_data_file(process_new_data, url):
             "ALT_Company_Name": str,
         },
     )
-
+    company_list()
     for row in sec_fsds_data:
         for item in row:
             if item == True:
@@ -145,13 +145,97 @@ def process_data_file(process_new_data, url):
     print(sec_fsds_array[0:, :1])
     if process_new_data == True:
         # Process num.txt
-
+        num_txt = pd.read_csv(
+            path / "data" / "sec_data_temp"/(url[66:72]/"num.txt"),
+            sep="\t",
+            dtype={
+                "adsh":str,
+                "tag":str,
+                "version":str,
+                "coreg":str,
+                "ddate":np.int32,
+                "qtrs":np.int8,
+                "uom":str,
+                "value":np.int32,
+                "footnote":str
+                }
+            )
         # Process pre.txt
-
+        pre_txt = pd.read_csv(
+            path / "data" / "sec_data_temp"/(url[66:72]/"pre.txt"),
+            sep="\t",
+            dtype={
+                "adsh":str,
+                "report":np.int8,
+                "line":np.int16,
+                "stmt":str,
+                "inpth":np.int8,
+                "rfile":str,
+                "tag":str,
+                "version":str,
+                "plabel":str,
+                "negating":np.int8                
+                }
+            )
         # Process sub.txt
-
+        sub_txt = pd.read_csv(
+            path / "data" / "sec_data_temp"/(url[66:72]/"sub.txt"),
+            sep="\t",
+            dtype={
+                "adsh":str,
+                "cik":np.int16,
+                "name":str,
+                "sic":np.int16,
+                "countryba":str,
+                "stprba":str,
+                "cityba":str,
+                "zipba":np.int32,
+                "bas1":str,
+                "bas2":str,
+                "baph":str,
+                "countryma":str,
+                "stprma":str,
+                "cityma":str,
+                "zipma":str,
+                "mas1":str,
+                "mas2":str,
+                "countryinc":str,
+                "stprinc":str,
+                "ein":np.int32,
+                "former":str,
+                "changed":np.int32,
+                "afs":str,
+                "wksi":np.int8
+                "fye":np.int16,
+                "form":str,
+                "period":np.int32,
+                "fy":np.int16,
+                "fp":str,
+                "filed":np.int32
+                "accepted":str,
+                "prevrpt":np.int8,
+                "detail":np.int8,
+                "instance":str,
+                "nciks":np.int8,
+                "aciks":str
+                }
+            )
         # Process tag.txt
-
+        tag_txt = pd.read_csv(
+            path / "data" / "sec_data_temp"/(url[66:72]/"tag.txt"),
+            sep="\t",
+            dtype={
+                "tag": str,
+                "version":str,
+                "custom":np.int16,
+                "abstract":np.int16,
+                "datatype":str,
+                "iord":str,
+                "crdr":str,
+                "tlabel":str,
+                "doc":str
+            }
+            )
         """
         #remove zip file
         os.remove("../data/sec_data_temp/"
@@ -171,9 +255,9 @@ def update_data():
         # download new package with function and unpack
         download_new_sec_file(next_sec_url)
         with zipfile.ZipFile(
-            path / "data" / "sec_data_temp/" + next_sec_url[66:72] + ".zip", "r",
+            path / "data" / "sec_data_temp/" / (next_sec_url[66:72] + ".zip"), "r",
         ) as zip_ref:
-            zip_ref.extractall(path / "data" / "sec_data_temp/" + next_sec_url[66:72])
+            zip_ref.extractall(path / "data" / "sec_data_temp/" / next_sec_url[66:72])
         add_file_to_sec_fsds(next_sec_url)
         check_new_file = True
         print("Processing lastest SEC data package...")
